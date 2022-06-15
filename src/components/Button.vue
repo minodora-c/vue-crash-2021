7f4f5b50bce52edeaf2678cff8fa6133c9622cb1
minodora-c <minodora.cristescu@campus.tu-berlin.de>
2022-06-15 12:42:13 +0200

    Vid until 40min

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="onClick()" :style="{background: color}" class="btn"> {{ text }}</button>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log("click")
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <h3>{{ task.text }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Tasks",
+  props: {
+    tasks: Array
+  }
+}
+</script>
